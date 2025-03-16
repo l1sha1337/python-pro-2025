4c0d062fbe3cb0d8670eb3791adb76142d23bde3
l3sha1337 <leshamal2012@gmail.com>
2025-03-16 18:26:16 +0700

    Add files via upload

---
+++ 16.03.25/16.03.25(1).py
@@ -0,0 +1,3 @@
+#import shutil
+#for i in range(1, 6):
+    #shutil.copy("data.txt",str(i) + "\data.txt")
---
+++ 16.03.25/16.03.25.py
@@ -0,0 +1,33 @@
+#3.for i in range(2, 26, 4):
+    #print(i)
+
+#4.for i in range(1, 55, 9):
+    #print(i)
+
+#5.for i in range (50, 30, -5):
+    #print(i)
+
+#6.question = int(input("Сколько раз написать привет? "))
+#for i in range(question):
+    #print("привет")
+
+#question = int(input("Напишите первое число "))
+#question2 = int(input("Напишите второе число "))
+#question3 = int(input("Напишите третье число "))
+#if question2 > question:
+    #if question3 > 0:
+        #for i in range(question, question2, question3):
+            #print(i)
+    #else:
+        #print("Что-то не так")
+#elif question2 < question:
+    #if question3 > 0:
+        #for i in range(question, question2, question3):
+            #print(i)
+    #else:
+        #print("Что-то не так")
+#count = 0
+#for i in range(1, 21, 1):
+    #count = count + i
+#print(count)
+
